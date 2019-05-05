92db3f0a4ed1d02bd38becfe2ba983a08cee9961
Adam Dangoor <adamdangoor@gmail.com>
2019-05-05 21:38:16 +0100

    Add flake8

--- src/strictest/lint/__init__.py
+++ src/strictest/lint/__init__.py
@@ -13,13 +13,14 @@ def lint_isort(skip):
     isort_args = [
         'isort',
         '--recursive',
+        '--quiet',
         '--check-only',
         '--multi-line=3',
         '--trailing-comma',
         '--dont-skip=__init__.py',
     ]
     for item in skip:
-        isort_args.append('--skip=' + item)
+        isort_args.append('--skip-glob=' + item)
     isort_result = subprocess.run(args=isort_args)
     if not isort_result.returncode == 0:
         sys.exit(isort_result.returncode)
@@ -31,6 +32,14 @@ def lint_check_manifest(skip):
         sys.exit(1)
 
 
+def lint_flake8(skip):
+    flake8_args = ['flake8']
+    if skip:
+        flake8_args.append('--exclude=' + ','.join(skip))
+    flake8_result = subprocess.run(args=flake8_args)
+    if not flake8_result.returncode == 0:
+        sys.exit(flake8_result.returncode)
+
 @click.command(name='lint')
 @click.option('--skip', multiple=True)
 def lint(skip) -> None:
@@ -39,3 +48,4 @@ def lint(skip) -> None:
     """
     lint_isort(skip=skip)
     lint_check_manifest(skip=skip)
+    lint_flake8(skip=skip)

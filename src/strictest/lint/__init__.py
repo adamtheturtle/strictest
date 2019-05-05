8ea628909bb8cae014c86ef4c4a77396b56d47b4
Adam Dangoor <adamdangoor@gmail.com>
2019-05-05 13:43:17 +0100

    Add isort

--- src/strictest/lint/__init__.py
+++ src/strictest/lint/__init__.py
@@ -2,10 +2,18 @@
 XXX
 """
 
+import subprocess
+import sys
+
 import click
 
-@click.group(name='lint')
+
+@click.command(name='lint')
 def lint() -> None:
     """
     XXX
     """
+    isort_args = ['isort', '--recursive', '--check-only']
+    isort_result = subprocess.run(args=isort_args)
+    if not isort_result.returncode == 0:
+        sys.exit(isort_result.returncode)

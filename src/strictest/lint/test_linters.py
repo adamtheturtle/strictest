fc1baa4e1e2dcf4c65837f7603a0202402f6a7e8
Adam Dangoor <adamdangoor@gmail.com>
2019-05-05 14:17:48 +0100

    Revert "Try using pytest as the runner"
    This reverts commit bf0f578d60329f7615df8093b88688cb9b3fb71e.

--- src/strictest/lint/test_linters.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-
-def test_isort():
-    isort_args = ['isort', '--recursive', '--check-only', '--multi-line=3', '--trailing-comma']
-    isort_result = subprocess.run(args=isort_args)
-    assert isort_result.returncode == 0
-

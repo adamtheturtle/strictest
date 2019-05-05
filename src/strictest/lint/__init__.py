a5a5f17ebb9610149780e32ecc6c89e388560325
Adam Dangoor <adamdangoor@gmail.com>
2019-05-05 14:36:50 +0100

    Add more isort args

--- src/strictest/lint/__init__.py
+++ src/strictest/lint/__init__.py
@@ -13,7 +13,14 @@ def lint() -> None:
     """
     XXX
     """
-    isort_args = ['isort', '--recursive', '--check-only', '--multi-line=3', '--trailing-comma']
+    isort_args = [
+        'isort',
+        '--recursive',
+        '--check-only',
+        '--multi-line=3',
+        '--trailing-comma',
+        '--dont-skip=__init__.py'
+    ]
     isort_result = subprocess.run(args=isort_args)
     if not isort_result.returncode == 0:
         sys.exit(isort_result.returncode)

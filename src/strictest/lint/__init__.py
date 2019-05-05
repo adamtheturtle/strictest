c6a90eb45d9f99ef24ca9f0263187d2fd22d9072
Adam Dangoor <adamdangoor@gmail.com>
2019-05-05 13:54:30 +0100

    Use a few options I like in isort

--- src/strictest/lint/__init__.py
+++ src/strictest/lint/__init__.py
@@ -13,7 +13,7 @@ def lint() -> None:
     """
     XXX
     """
-    isort_args = ['isort', '--recursive', '--check-only']
+    isort_args = ['isort', '--recursive', '--check-only', '--multi-line=3', '--trailing-comma']
     isort_result = subprocess.run(args=isort_args)
     if not isort_result.returncode == 0:
         sys.exit(isort_result.returncode)
